ceb8f1a6471c6e6e2dc2a4a8c043d3c796f6ab41
Ahmad Mash <wolfmashagbah@hotmail.com>
2023-10-07 20:45:47 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
-// Code your solution in this file!
+
+function distanceFromHqInBlocks(n) {
+    if (n >= 42) {
+    n = n - 42
+    }
+    else {
+        n = 42 - n
+    }
+
+    return n
+}
+
+function distanceFromHqInFeet(n) {
+    if (n >= 42) {
+        n = n - 42
+        }
+        else {
+            n = 42 - n
+        }
+        return n * 264
+ }
+
+function distanceTravelledInFeet(a, b) {
+
+    if (a > b) {
+        a = a - b
+    }
+    else {
+            a = b - a
+        }
+        return a * 264
+}
+
+function calculatesFarePrice(a, b) {
+
+    if (a > b) {
+        a = a - b
+    }
+    else {
+            a = b - a
+        }
+    a = a * 264
+
+    if (a < 400) {
+        return 0
+    }
+    else if (a >= 400 && a <= 2000) {
+        a = (a - 400) * 0.02
+        return a
+    }
+    else if(a > 2000 && a <= 2500) {
+        a = 25
+        return a
+    }
+    else if (a > 2500) {
+       let cool = 'cannot travel that far'
+        return cool
+}
+return a
+}
